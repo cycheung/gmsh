faff57a45eb3312e555c7eee375f05e34027bbaf
henrotte <henrotte@59c8cabf-10d6-4a28-b43f-3936b85956c4>
2011-11-07 11:09:34 +0000

    tested version
    git-svn-id: https://geuz.org/svn/gmsh/trunk@10374 59c8cabf-10d6-4a28-b43f-3936b85956c4

--- projects/onelab/METAMODELS/CORE/metamodel.cpp
+++ projects/onelab/METAMODELS/CORE/metamodel.cpp
@@ -4,13 +4,17 @@
 
 std::vector<double> variables(5);
 std::string modelName="test";
-PromptUser *promptMe = new PromptUser("shell");
+PromptUser *OL = new PromptUser("shell");
 EncapsulatedGmsh *myMesher = new EncapsulatedGmsh("gmsh");
 EncapsulatedGetdp *mySolver = new EncapsulatedGetdp("getdp");
 
 
 int metamodel(int modelNumber){
   switch (modelNumber){
+  case 1:
+    OL->setNumber("1Geometry/2Val_Rext",0.5);
+    simulation();
+    break;
   case 0:
   default:
     simulation();
@@ -20,30 +24,32 @@ int metamodel(int modelNumber){
 
 
 int simulation(){
-
-
   // std::string sockname("localhost:");
   // const char *port = strstr(sockname.c_str(), ":");
   // int portno = atoi(port + 1); 
   // std::cout << "sockname=<" << sockname << "> portno=" << portno << std::endl;
   // exit(1);
   
-  printf("FHF server has %d Clients\n", onelab::server::instance()->getNumClients());
+  //printf("FHF server has %d Clients\n", onelab::server::instance()->getNumClients());
 
-
-  myMesher->analyze(modelName); //populate parameterspace
-  promptMe->menu("About to mesh",modelName,myMesher->getName());
+  if (OL->getInteractivity()) {
+    myMesher->analyze(modelName); //populate parameterspace
+    OL->menu("About to mesh",modelName,myMesher->getName());
+  }
   myMesher->run("-2", modelName);
 
   mySolver->analyze(modelName); //populate parameterspace
-  promptMe->menu("About to solve",modelName,mySolver->getName());
+  if (OL->getInteractivity()) {
+    OL->menu("About to solve",modelName,mySolver->getName());
+  }
   mySolver->sol("",modelName);
 
-  promptMe->menu("About to post",modelName,mySolver->getName());
+  if (OL->getInteractivity()) {
+    OL->menu("About to post",modelName,mySolver->getName());
+  }
 
   mySolver->pos("",modelName);
 
-  promptMe->menu("About to leave",modelName,"");
-
-  return 1;
+  if (checkWhetherModified("b_phi.pos"))
+    std::cout << "Simulation completed successfully" << std::endl;
 }
