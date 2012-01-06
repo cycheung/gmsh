c4c4f35bf3d7b852d745aa7ed37e44a8f6372824
henrotte <henrotte@59c8cabf-10d6-4a28-b43f-3936b85956c4>
2012-01-06 15:36:45 +0000

    install stuff
    git-svn-id: https://geuz.org/svn/gmsh/trunk@10874 59c8cabf-10d6-4a28-b43f-3936b85956c4

--- projects/onelab/olmake.sh
+++ projects/onelab/olmake.sh
@@ -26,8 +26,6 @@ echo ""
 echo "ONELAB: compile the console (non gui) onelab loader"
 g++ -I $ONELAB_DIR -I $COMMON_DIR $COMMON_DIR/OS.cpp $ONELAB_DIR/OnelabClients.cpp $ONELAB_DIR/OnelabMessage.cpp $ONELAB_DIR/loader.cpp -o loader
 
-exit 
-
 echo "ONELAB: compile the metamodels"
 
 cd METAMODELS
