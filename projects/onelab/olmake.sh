b32d7a6a2f3a0e15e22841e7422a77c514e5d08b
henrotte <henrotte@59c8cabf-10d6-4a28-b43f-3936b85956c4>
2012-01-06 15:35:57 +0000

    install stuff
    git-svn-id: https://geuz.org/svn/gmsh/trunk@10873 59c8cabf-10d6-4a28-b43f-3936b85956c4

--- projects/onelab/olmake.sh
+++ projects/onelab/olmake.sh
@@ -1,5 +1,23 @@
+#!/bin/bash
+
 #The variable 'GMSH_DIR' must be set to the home dir of gmsh on your system
 #e.g. export GMSH_DIR=$HOME/SOLVERS/gmsh   
+
+if [ -z "$GMSH_DIR" ]; then
+ echo "The variable 'GMSH_DIR' must be set to the home dir of gmsh on your system"
+ echo "e.g. export GMSH_DIR=$HOME/SOLVERS/gmsh"
+ exit
+fi
+
+if [ -z  "$`which gmsh`" ]; then
+ echo "The command 'gmsh' is not defined systemwide"
+fi
+if [ -z  "`which getdp`" ]; then
+ echo "The command 'getdp' is not defined systemwide"
+fi
+if [ -z  "`which elmer`" ]; then
+ echo "The command 'elmer' is not defined systemwide"
+fi
 
 export COMMON_DIR=$GMSH_DIR/Common
 export ONELAB_DIR=$GMSH_DIR/projects/onelab
@@ -7,6 +25,8 @@ export ONELAB_DIR=$GMSH_DIR/projects/onelab
 echo ""
 echo "ONELAB: compile the console (non gui) onelab loader"
 g++ -I $ONELAB_DIR -I $COMMON_DIR $COMMON_DIR/OS.cpp $ONELAB_DIR/OnelabClients.cpp $ONELAB_DIR/OnelabMessage.cpp $ONELAB_DIR/loader.cpp -o loader
+
+exit 
 
 echo "ONELAB: compile the metamodels"
 
