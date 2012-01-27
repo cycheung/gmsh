4869a93e388cd2e74dac0bdfdf300a9bc7d6db1d
henrotte <henrotte@59c8cabf-10d6-4a28-b43f-3936b85956c4>
2012-01-27 14:26:21 +0000

    git-svn-id: https://geuz.org/svn/gmsh/trunk@11142 59c8cabf-10d6-4a28-b43f-3936b85956c4

---
+++ projects/onelab/SOLVERS/onelab.cpp
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string>
+#include "OnelabClients.h"
+
+PromptUser *OL = new PromptUser("onelab");
+void MetaModel::analyze(){
+  Msg::Info("Metamodel::analyze <%s>",getName().c_str());
+  simpleCheck();
+  //std::cout << OL->showParamSpace() << std::endl;
+}
+
+void MetaModel::compute(){
+  Msg::Info("Metamodel::compute <%s>",getName().c_str());
+  simpleCompute();
+  //std::cout << OL->showParamSpace() << std::endl;
+  
+  GmshDisplay(Msg::loader,Msg::GetOnelabString("Arguments/Filename"),Msg::GetOnelabChoices("Display/InputFiles"));
+
+
+  std::cout << "Simulation completed successfully" << std::endl;
+}
