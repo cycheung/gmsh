1f04a9983ce6e0b33fb109e49c178797d83c1310
henrotte <henrotte@59c8cabf-10d6-4a28-b43f-3936b85956c4>
2012-01-31 11:22:57 +0000

    PostArray
    git-svn-id: https://geuz.org/svn/gmsh/trunk@11170 59c8cabf-10d6-4a28-b43f-3936b85956c4

--- projects/onelab/SOLVERS/onelab.cpp
+++ projects/onelab/SOLVERS/onelab.cpp
@@ -7,15 +7,30 @@ void MetaModel::analyze(){
   Msg::Info("Metamodel::analyze <%s>",getName().c_str());
   simpleCheck();
   //std::cout << OL->showParamSpace() << std::endl;
+  std::cout << OL->showClientStatus() << std::endl;
 }
 
 void MetaModel::compute(){
   Msg::Info("Metamodel::compute <%s>",getName().c_str());
   simpleCompute();
-  //std::cout << OL->showParamSpace() << std::endl;
-  
   GmshDisplay(Msg::loader,Msg::GetOnelabString("Arguments/FileName"),Msg::GetOnelabChoices("GmshMerge/InputFiles"));
-
-
-  std::cout << "Simulation completed successfully" << std::endl;
+  std::cout << "Simulation completed successfully..." << std::endl;
+  
+  std::vector<std::string> choices;
+  std::vector<onelab::string> strings;
+  get(strings,"PostArray");
+  if(strings.size()){
+    choices = strings[0].getChoices();
+    int nb=0;
+    onelab::number o;
+    while( 4*(nb+1) <= choices.size()){
+      Msg::Info("PostArray <%s>",choices[4*nb+3].c_str());
+      int lin= atof(choices[4*nb+1].c_str());
+      int col= atof(choices[4*nb+2].c_str());
+      o.setName(choices[4*nb+3]);
+      o.setValue(find_in_array(lin,col,read_array(choices[4*nb],' ')));
+      set(o);
+      nb++;
+    }
+  }
 }
