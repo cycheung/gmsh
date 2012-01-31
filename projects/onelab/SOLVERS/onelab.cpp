8daa05c5422b1f2314fd05474199e382b1483c36
henrotte <henrotte@59c8cabf-10d6-4a28-b43f-3936b85956c4>
2012-01-31 14:43:31 +0000

    git-svn-id: https://geuz.org/svn/gmsh/trunk@11175 59c8cabf-10d6-4a28-b43f-3936b85956c4

--- projects/onelab/SOLVERS/onelab.cpp
+++ projects/onelab/SOLVERS/onelab.cpp
@@ -15,22 +15,5 @@ void MetaModel::compute(){
   simpleCompute();
   GmshDisplay(Msg::loader,Msg::GetOnelabString("Arguments/FileName"),Msg::GetOnelabChoices("GmshMerge/InputFiles"));
   std::cout << "Simulation completed successfully..." << std::endl;
-  
-  std::vector<std::string> choices;
-  std::vector<onelab::string> strings;
-  get(strings,"PostArray");
-  if(strings.size()){
-    choices = strings[0].getChoices();
-    int nb=0;
-    onelab::number o;
-    while( 4*(nb+1) <= choices.size()){
-      Msg::Info("PostArray <%s>",choices[4*nb+3].c_str());
-      int lin= atof(choices[4*nb+1].c_str());
-      int col= atof(choices[4*nb+2].c_str());
-      o.setName(choices[4*nb+3]);
-      o.setValue(find_in_array(lin,col,read_array(choices[4*nb],' ')));
-      set(o);
-      nb++;
-    }
-  }
+  PostArray(Msg::GetOnelabChoices("PostArray"));
 }
