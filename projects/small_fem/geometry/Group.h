084063f2361a9940b3f0d828d6fb253af5713223
marsic <marsic@59c8cabf-10d6-4a28-b43f-3936b85956c4>
2012-06-28 15:58:32 +0000

    Interface for Groups
    git-svn-id: https://geuz.org/svn/gmsh/trunk@12538 59c8cabf-10d6-4a28-b43f-3936b85956c4

---
+++ projects/small_fem/geometry/Group.h
@@ -0,0 +1,17 @@
+#ifndef _GROUP_H_
+#define _GROUP_H_
+
+/**
+   @interface Group
+   @brief A group of elements
+
+   This is the interface allowing @em access to a group
+   of elements.
+ */
+
+class Group{
+ public:
+  
+};
+
+#endif
