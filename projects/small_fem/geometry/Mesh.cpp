e920358df0b1c0e93e2ee3d1831b6d68bea63b1b
marsic <marsic@59c8cabf-10d6-4a28-b43f-3936b85956c4>
2012-06-29 12:47:27 +0000

    continued
    git-svn-id: https://geuz.org/svn/gmsh/trunk@12560 59c8cabf-10d6-4a28-b43f-3936b85956c4

--- projects/small_fem/geometry/Mesh.cpp
+++ projects/small_fem/geometry/Mesh.cpp
@@ -1,18 +1,30 @@
 #include "Mesh.h"
 #include "Exception.h"
 
+#include <iostream>
+
 using namespace std;
 
 Mesh::Mesh(const std::string fileName){ 
-  model = new GModel("SmallFEM");
-  
+  // Alloc Memory //
+  model       = new GModel("SmallFEM");
+  physToGroup = new map<int, Group*>;
+
+  // Read Mesh //
   if(!model->readMSH(fileName))
     throw Exception("Can't open file: %s", fileName.c_str());
 
+  // Get Entities //
+  vector<GEntity*> entity;
+  model->getEntities(entity);
+
+  for(int i = 0; i < entity.size(); i++)
+    cout << entity[i]->dim() << endl;
 }
  
 Mesh::~Mesh(void){
   delete model;
+  delete physToGroup;
 }
 
 const vector<pair<int, Group*> > Mesh::getAllGroups(void) const{
