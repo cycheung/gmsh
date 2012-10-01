65b1cdd87126e0b2bccad554fcc8f6660c84aeae
marsic <marsic@59c8cabf-10d6-4a28-b43f-3936b85956c4>
2012-10-01 09:06:02 +0000

    Convergence
    git-svn-id: https://geuz.org/svn/gmsh/trunk@13411 59c8cabf-10d6-4a28-b43f-3936b85956c4

--- projects/small_fem/simulation/ConvPoisson.cpp
+++ projects/small_fem/simulation/ConvPoisson.cpp
@@ -124,17 +124,17 @@ fullMatrix<double> l2(fullMatrix<vector<double> >& fem, vector<double>& ana){
   // Norm of Analytic Solution //
   double anaNorm = 0;
   for(unsigned int k = 0; k < nNode; k++)
-    anaNorm += ana[k];// * ana[k];
+    anaNorm += ana[k] * ana[k];
   
-  //anaNorm = sqrt(anaNorm);
+  anaNorm = sqrt(anaNorm);
   
   // Norm of FEM Error //
   for(unsigned int i = 0; i < nOrder; i++){
     for(unsigned int j = 0; j < nMesh; j++){
       for(unsigned int k = 0; k < nNode; k++)
-	res(i, j) += (ana[k] - fem(i, j)[k]);// * (ana[k] - fem(i, j)[k]);
+	res(i, j) += (ana[k] - fem(i, j)[k]) * (ana[k] - fem(i, j)[k]);
       
-      res(i, j) = res(i, j) / anaNorm;//sqrt(res(i, j)) / anaNorm;
+      res(i, j) = sqrt(res(i, j)) / anaNorm;
     }
   }
 
